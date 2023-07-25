ba020d750749a3a61299dc7e8cedb6cef68453d6
Nertwy <deniskabos@gmail.com>
2023-07-25 23:23:19 +0300

    Created draft example of language model

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -1,14 +1,38 @@
 import { useState } from "react";
-
+import { useTranslation, Trans } from "react-i18next";
+const lngs = {
+  en: { nativeName: "English" },
+  uk: { nativeName: "Ukrainian" },
+};
 const NavBar = () => {
+  const { t, i18n } = useTranslation();
   const [isMenuOpen, setMenuOpen] = useState(false);
 
+  const changeLang = () => {
+    {
+      Object.keys(lngs).map((lng) => (
+        <button
+          key={lng}
+          style={{
+            fontWeight: i18n.resolvedLanguage === lng ? "bold" : "normal",
+          }}
+          type="submit"
+          onClick={() => i18n.changeLanguage(lng)}
+        >
+          {lngs[lng as keyof typeof lngs].nativeName}
+        </button>
+      ));
+    }
+  };
   const handleMenuToggle = () => {
     setMenuOpen(!isMenuOpen);
   };
 
   return (
     <nav className="h-24 md:h-auto flex flex-col xl:pl-20 md:flex-row items-center justify-between bg-sky-100 text-gray-700 md:pl-0 xl:pr-20">
+      <button onClick={() => i18n.changeLanguage("uk")}>
+        CHANGE LANGUAGE
+      </button>
       <a className="text-3xl font-bold mb-4  md:mb-0 font-sora  italic text-gray-700 ">
         Nertwy
       </a>
@@ -49,7 +73,7 @@ const NavBar = () => {
       >
         <li className="px-4 py-2 mb-4 md:mb-0 md:mr-13">
           <a className="text-3xl hover:text-black" href="#">
-            Home
+            {t("description.part1")}
           </a>
         </li>
         <li className="px-4 py-2 mb-4 md:mb-0 md:mr-13">
