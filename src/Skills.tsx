10fde060cb264355edf37831b304908e60b1b31c
Nertwy <deniskabos@gmail.com>
2023-08-24 21:17:16 +0300

    Added Skills Animation

--- src/Skills.tsx
+++ src/Skills.tsx
@@ -7,6 +7,8 @@ import Image6 from "./assets/Postgresql.png";
 import Image7 from "./assets/Prisma.png";
 import Image8 from "./assets/Typescript.png";
 import { useTranslation } from "react-i18next";
+import { FC } from "react";
+const logos = [Image1, Image2, Image3, Image4, Image5, Image6, Image7, Image8];
 const Skills = () => {
   const { t } = useTranslation();
   return (
@@ -17,17 +19,41 @@ const Skills = () => {
       <h2 className="text-5xl pb-8 font-bold text-gray-700">
         {t("description.skillsPage_Header2")}
       </h2>
-      <div className="grid gap-4 grid-cols-2 md:flex w-full place-items-center sm:grid-cols-3 justify-center">
-        <img src={Image1} className="h-20" />
-        <img src={Image2} className="h-20" />
-        <img src={Image3} className="h-20" />
-        <img src={Image4} className="h-20" />
-        <img src={Image5} className="h-20 " />
-        <img src={Image6} className="h-20 " />
-        <img src={Image7} className="h-20 " />
-        <img src={Image8} className="h-20 " />
-      </div>
+      <LogoCarousel logos={logos} />
     </section>
   );
 };
 export default Skills;
+
+type LogoCarouselProps = {
+  logos: string[];
+};
+
+const LogoCarousel: FC<LogoCarouselProps> = ({ logos }) => {
+  const phone = window.innerWidth < 920 ? true : false;
+  return phone ? (
+    <div className="grid gap-4 grid-cols-2 md:flex w-full place-items-center sm:grid-cols-3 justify-center">
+      <img src={Image1} className="h-20" />
+      <img src={Image2} className="h-20" />
+      <img src={Image3} className="h-20" />
+      <img src={Image4} className="h-20" />
+      <img src={Image5} className="h-20 " />
+      <img src={Image6} className="h-20 " />
+      <img src={Image7} className="h-20 " />
+      <img src={Image8} className="h-20 " />
+    </div>
+  ) : (
+    <div className="w-full overflow-hidden whitespace-nowrap relative px-7 logos">
+      <div className="logo_items inline-flex w-full justify-around">
+        {logos.map((item) => (
+          <img src={item} className="md:h-24" />
+        ))}
+      </div>
+      <div className="logo_items inline-flex justify-around w-full">
+        {logos.map((item) => (
+          <img src={item} className="h-24" />
+        ))}
+      </div>
+    </div>
+  );
+};
